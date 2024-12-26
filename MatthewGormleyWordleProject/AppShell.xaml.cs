fc8b9a2003cfa24e0678570c5ede33e804a578dd
MatthewGormley05 <matthewgormley005@gmail.com>
2024-12-26 13:53:54 +0000

    Add project files.

---
+++ MatthewGormleyWordleProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MatthewGormleyWordleProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MatthewGormleyWordleProject/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MatthewGormleyWordleProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
